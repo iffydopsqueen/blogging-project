bda5e0ffe85dbb6067cd1ce2130e8cb9fd100ffe
Iffy Eleweke <omaleweks@gmail.com>
2024-02-21 15:24:17 -0500

    Added the basic structure of the website

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 // Routes
 router.get('', (req, res) => {
-    res.send("Hello World!");
+    // res.send("Hello World!");  --> Not NEEDED since we now use a templating engine 
+    const locals = {
+        title: "My Blogging Project",
+        description: "The Blog created with Express, NodeJS & MongoDB"
+    }
+
+    res.render('index', locals);   // Adding the title & description to the Home page 
+});
+
+router.get('/about', (req, res) => {
+    // res.send("Hello World!");  --> Not NEEDED since we now use a templating engine 
+    res.render('about');
 });
 
 // Export router else the application doesn't work
