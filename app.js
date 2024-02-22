4d8ae089135287b219bd5c61ca14ef08413c4ed7
Iffy Eleweke <omaleweks@gmail.com>
2024-02-22 13:36:37 -0500

    Added database connection file

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
+const connectDB = require('./server/config/db');
+
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
