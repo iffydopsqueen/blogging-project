951e974a161d87f108d02d390c33d32b94a415f2
Iffy Eleweke <omaleweks@gmail.com>
2024-02-22 18:04:30 -0500

    Confirmed that the registeration for the admin works

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');   // store session when we log in
+const MongoStore = require('connect-mongo');
 
 const connectDB = require('./server/config/db');
 
@@ -14,6 +17,19 @@ connectDB();
 // Middleware for search 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.DATABASE_URI
+  }),
+    // cookie: { 
+    //     maxAge: new Date ( Date.now() + (3600000) ) 
+    // }
+}));
 
 app.use(express.static('public'));
 
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');   // encrypt & decrypt password 
+const jwt = require('jsonwebtoken');
 
 const adminLayout = '../views/admin/layout';
 /**
@@ -20,6 +22,48 @@ router.get('/admin', async (req, res) => {
             locals,
             layout: adminLayout
         });   
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * POST /
+ * Admin - Check Login
+*/
+
+router.post('/admin', async (req, res) => {
+    try {
+        const { username, password } = req.body;  // get the username and password from the form
+        
+        res.render('admin/index', {
+            locals,
+            layout: adminLayout
+        });   
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * POST /
+ * Admin - Register
+*/
+
+router.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;  // get the username and password from the form
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        try {
+            const user = await User.create({ username, password:hashedPassword });
+            res.status(201).json({ message: 'User Created', user });    // testing purposes
+        } catch (error) {
+            if (error.code === 11000) {
+                res.status(409).json({ message: 'User already in use' });
+            }
+            res.status(500).json({ message: 'Internal server error' });
+        }
     } catch (error) {
         console.log(error);
     }
